4dc3a17d5686e78455403ec7ad5c4896e007dcc1
Luis Del Águila <luisdelaguila@outlook.com>
2017-12-17 18:39:57 -0600

    Allow to delete registered books

--- stores/rows.js
+++ stores/rows.js
@@ -4,15 +4,36 @@ module.exports = store
 
 function store (state, emitter) {
   state.rows = []
+  var id = 0
 
   emitter.on('DOMContentLoaded', function () {
     emitter.on('rows:add', function () {
-      state.rows.push({ id: state.rows.length })
+      state.rows.push({ id: id })
+      id += 1
+
       emitter.emit(state.events.RENDER)
     })
 
-    emitter.on('input', function (row) {
-      state.rows[row.index] = row
+    emitter.on('rows:delete', function (id) {
+      var foundIndex = findRow(state.rows, id)
+
+      if (foundIndex !== -1) {
+        state.rows.splice(foundIndex, 1)
+
+        emitter.emit(state.events.RENDER)
+      }
+    })
+
+    emitter.on('input', function (newRow) {
+      var foundIndex = findRow(state.rows, newRow.id)
+
+      if (foundIndex !== -1) {
+        state.rows[foundIndex] = newRow
+      }
     })
   })
+}
+
+function findRow (rows, id) {
+  return rows.findIndex(function (row) { return row.id === id })
 }
--- templates/rows.js
+++ templates/rows.js
@@ -6,35 +6,39 @@ var extend = require('extend')
 module.exports = template
 
 function template (state, emit) {
-  return state.rows.map(function (row, i) {
+  return state.rows.map(function (row) {
     return html`<tr>
       <td>
-        <input class="ba" id="title${i}" value="${str(row['title'])}"
-          onchange=${handleInput('title', row, i)}/>
+        <input class="ba" id="title${row.id}" value="${str(row['title'])}"
+          onchange=${handleInput('title', row)}/>
       </td>
       <td>
-        <input class="ba" id="author${i}" value="${str(row['author'])}"
-          onchange=${handleInput('author', row, i)}/>
+        <input class="ba" id="author${row.id}" value="${str(row['author'])}"
+          onchange=${handleInput('author', row)}/>
       </td>
       <td>
-        <input class="ba" id="year${i}" value="${str(row['year'])}"
-          onchange=${handleInput('year', row, i)}/>
+        <input class="ba" id="year${row.id}" value="${str(row['year'])}"
+          onchange=${handleInput('year', row)}/>
       </td>
       <td>
-        <input class="ba" id="ISBN${i}" value="${str(row['ISBN'])}"
-          onchange=${handleInput('ISBN', row, i)}/>
+        <input class="ba" id="ISBN${row.id}" value="${str(row['ISBN'])}"
+          onchange=${handleInput('ISBN', row)}/>
       </td>
       <td>
-        <input class="ba" id="genre${i}" value="${str(row['genre'])}"
-          onchange=${handleInput('genre', row, i)}/>
+        <input class="ba" id="genre${row.id}" value="${str(row['genre'])}"
+          onchange=${handleInput('genre', row)}/>
       </td>
       <td>
-        <input class="ba" id="stock${i}" value="${str(row['stock'])}"
-          onchange=${handleInput('stock', row, i)}/>
+        <input class="ba" id="stock${row.id}" value="${str(row['stock'])}"
+          onchange=${handleInput('stock', row)}/>
       </td>
       <td>
-        <input class="ba" id="price${i}" value="${str(row['price'])}"
-          onchange=${handleInput('price', row, i)}/>
+        <input class="ba" id="price${row.id}" value="${str(row['price'])}"
+          onchange=${handleInput('price', row)}/>
+      </td>
+      <td>
+        <input type="button" id="delete${row.id}" value="❌"
+          onclick=${handleDeleteClick}/>
       </td>
     </tr>`
   })
@@ -43,14 +47,18 @@ function template (state, emit) {
     return x || ''
   }
 
-  function handleInput (columnName, row, index) {
-    return function (/* event */) {
+  function handleInput (columnName, row) {
+    return function (event) {
       var newRow = extend(row, {
-        index: index,
-        [columnName]: document.getElementById(`${columnName}${index}`).value
+        [columnName]: document.getElementById(`${columnName}${row.id}`).value
       })
 
       emit('input', newRow)
     }
+  }
+
+  function handleDeleteClick (e) {
+    // Get row ID (number) from target's ID (string)
+    emit('rows:delete', +e.target.id.replace('delete', ''))
   }
 }
