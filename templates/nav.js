5655e0ca5ce8f5278247d0c60361ab33a898012d
Luis Del Águila <luisdelaguila@outlook.com>
2017-12-27 22:54:58 -0600

    Improve navigation links

--- templates/nav.js
+++ templates/nav.js
@@ -6,17 +6,17 @@ function view (state, emit) {
   var type = state.params.type
 
   return html`<nav class="pa3 pa4-ns">
-    <a class="link dim ${!type ? 'black b' : 'gray'} f6 f5-ns dib mr3"
+    <a class="link ${!type ? 'black b' : 'gray dim'} f6 f5-ns dib mr3"
       href="/" title="Inicio">
       Inventario
     </a>
 
-    <a class="link dim ${type === 'in' ? 'black b' : 'gray'} f6 f5-ns dib mr3"
+    <a class="link ${type === 'in' ? 'black b' : 'gray dim'} f6 f5-ns dib mr3"
       href="/register/in" title="Registrar entradas">
       Registrar entradas
     </a>
 
-    <a class="link dim ${type === 'out' ? 'black b' : 'gray'} f6 f5-ns dib mr3"
+    <a class="link ${type === 'out' ? 'black b' : 'gray dim'} f6 f5-ns dib mr3"
       href="/register/out" title="Registrar salidas">
       Registrar salidas
     </a>
